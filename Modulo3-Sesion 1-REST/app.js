782b4be060249655ca91d7823d8452ef70afe1fa
Fernanda Romero <140003497+Fer2345Romero@users.noreply.github.com>
2024-06-03 11:12:55 -0600

    Primera API

---
+++ Modulo3-Sesion 1-REST/app.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const app= express()
+const port = 3000
+
+app.use(express.json())
+
+let tasks =[]
+
+app.get('/tasks', (req, res)=>{
+    res.json(tasks)
+})
+
+app.post('/tasks',(req, res)=>{
+    const newtask={
+        id:tasks.length +1,
+        title: req.body.title,
+        completed:false
+    }
+    tasks.push(newtask)
+    res.status(201).json(newtask)
+})
+
+app.get('/tasks/:id', (req, res)=>{
+    const taskid = parseInt(req.params.id)
+    const task = tasks.find(t=>t.id === taskid)
+    if(task){
+        res.json(task)
+    }else{
+        res.status(404).send('No se encuentra la tarea')
+    }
+})
+
+app.put('/tasks/:id', (req, res)=>{
+    const taskid = parseInt(req.params.id)
+    const task = tasks.find(t=> t.id === taskid)
+    if(task)
+        {
+            task.title=req.body.title || task.title
+            task.completed= req.body.completed !== undefined ? req.body.completed: task.completed
+            res.json(task)
+        }else{
+            res.status(404).send('No se acualizo la tarea')
+        }
+})
+
+app.delete('/tasks/:id', (req, res)=>{
+    const taskid = parseInt(req.params.id)
+    const taskin = tasks.findIndex(t => t.id === taskid)
+    if(taskin !== -1){
+        tasks.splice(taskin, 1)
+        res.status(204).send('Registro eliminado')
+    }else{
+        res.status(404).send('No se elimino la tarea')
+    }
+})
+
+app.listen(port, () =>{
+    console.log(`servidor corriendo en la url http://localhost:${port}`)
+})
